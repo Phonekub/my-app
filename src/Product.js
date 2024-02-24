1ee8662328bdbd344771812c909844e97dfd2163
Supaiwt <phone2546.pp@gmail.com>
2024-02-24 23:20:24 +0700

    showproduct

--- src/Product.js
+++ src/Product.js
@@ -1,6 +1,9 @@
-import { useState,useEffect } from "react";
+import React,{ useState,useEffect } from "react";
 import axios from "axios";
 export default function Product(){
+    const baseURL="http://127.0.0.1:5000/products";
+    const myInputRef1= React.createRef();
+    const myInputRef2= React.createRef();
     const [product,setProduct]=useState([])
     useEffect(()=>{
         console.log("request to api")
@@ -10,11 +13,43 @@ export default function Product(){
             console.error('Error fetching data:', error);
           })
     },[])
-    const productList=product.map(p=><li key={p.id}>{p.id} 
-    {p.name} <img src={p.img}/> {p.price}</li>)
-    return (<>
-        <ul>
-            {productList}
-        </ul>
-    </>)
+    const onDeleteProduct=(id)=>{
+        console.log(id);
+        axios.delete(baseURL+"/api/products"+id).then((response)=>{
+            setProduct(response.data);
+        });
+    }
+    
+    // const onAddProduct=()=>{
+    //     console.log(myInputRef1.current.value);
+    //     console.log(myInputRef2.current.value);
+    //     const data={
+    //         name:myInputRef1.current.value;
+    //         price:myInputRef2.current.value;
+    //     }
+
+        
+    // }
+
+   const show_product = product.map((item)=>{
+    return (<tr><td>{item.id}</td><td>{item.name}</td><td>{item.price}</td>
+        <td><button onClick>delete</button></td>
+        <td><button onClick>ok</button></td>
+
+    </tr>)
+   })
+    return (<div>       
+        <table border='1'>
+            <thead>
+                <tr>    
+                    <td>id</td><td>name</td><td>price</td><td></td>
+                </tr>
+            </thead>
+            <tbody>{show_product}</tbody>
+        </table>
+            Product name:<input type='text' name='product_name' ref={myInputRef1}/> 
+            <br/> Price <input type='text' name='product_price' ref={myInputRef2}/> 
+            <button onClick>Add</button>
+    </div>)
+
 }
