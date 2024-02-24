01b4eabab895df2d906df55871768a0196291b41
Supaiwt <phone2546.pp@gmail.com>
2024-02-24 22:11:01 +0700

    Front

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Hello from './Hello';
+import Product from './Product';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <Hello/> */}
+    <Product/>
   </React.StrictMode>
 );
 
